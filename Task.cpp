f8c6d04958021bbc93ef91c01b0cc4e1efd1c488
SMacC <C24393863@myTUDublin.ie>
2025-11-24 09:35:02 +0000

    Added Constructor and Fixed Destructor

--- Task.cpp
+++ Task.cpp
@@ -1,8 +1,15 @@
 #include "Task.h"
-Task::Task(string name, string due, string desc, string progress, string user);
-
+Task::Task(string name, string due, string desc, string progress, string user)
+{
 string name = name;
 string due = due_date;
 string desc = description;
 string progress = progress_status;
 string user = user;
+}
+
+Task::~Task(){
+
+std::cout << "Destructor for task called" <<std::endl;
+
+}
