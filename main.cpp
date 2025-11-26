df274c50f42e651a9b9ab7f706248187f804b81d
SMacC <C24393863@myTUDublin.ie>
2025-11-26 11:35:46 +0000

    Added User Selection

--- main.cpp
+++ main.cpp
@@ -4,8 +4,30 @@
 #include "User.h"
 using namespace std;
 int main(){
-cout <<5;
-
+int x = 5;
+cout <<"Welcome to the Task Manager!" <<endl;
+cout <<"Select an account:" <<endl;
+cin >> x ; 
+switch(x){
+    case 1:
+    cout <<"User 1 selected"<<endl;
+    break;
+    case 2:
+    cout <<"User 2 selected"<<endl;
+    break;
+    case 3:
+    cout <<"User 3 selected"<<endl;
+    break;
+    case 4:
+    cout <<"User 4 selected"<<endl;
+    break;
+    case 5:
+    cout <<"User 5 selected"<<endl;
+    break;
+    default:
+    cout << "Please Choose a Valid User"<<endl;
+    break;
+}
 
     return 0;
 }
