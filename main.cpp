6918243f62056a8afc706c2ab48650529efa976f
SMacC <C24393863@myTUDublin.ie>
2025-11-27 22:29:16 +0000

    Added comments and fixed minor bug

--- main.cpp
+++ main.cpp
@@ -3,13 +3,13 @@
 #include "Task.h"
 #include "User.h"
 using namespace std;
-
+int b{};
 int x{};
 int main(){
 cout <<"Welcome to the Task Manager!" <<endl;
-cout <<"Select an account:" <<endl;
+cout <<"Select an account-enter a number 1-5:" <<endl;//This needs to be revised
 cin >> x ; 
-switch(x){
+switch(x){//user selection
     case 1:
     cout <<"User 1 selected"<<endl;
     break;
@@ -29,10 +29,11 @@ switch(x){
     cout << "Please Choose a Valid User"<<endl;
     break;
 }
-  User emp[5]; 
-  Task foo[5];
-  int n, i;
+    User emp[5]; //array of users
+    Task foo[1000];//array of tasks
+    int n, i;
     emp[x].getdata();//getting user account creation
+    //need to add a way to check if account exists
   //use this for login maybe
 
 
@@ -46,28 +47,20 @@ switch(x){
 
 
 for(int i{}; i<1; ){//looping the switch statement
-    int b{1};
+    
 cout <<"Select an option: 1 to view all tasks, 2 to add a task, 3 to view urgent tasks and 4 to exit"<<endl;
 cin >> x ; 
 switch(x){
-    case 1:
-  for(int j = 0; j < 5; j++) 
-    foo[j].putdata();
-    
+    case 1://see statement below
     break;
-    case 2:
-    
-        b++;
+    case 2://see statement below
+    b++;//updating total number of tasks
     cout <<"Add Task"<<endl;
-     
-   
-      
-    
     break;
     case 3:
-    cout <<"View Urgent Tasks"<<endl;
+    cout <<"View Urgent Tasks"<<endl;//no functionality. Will change to edit tasks
     break;
-    case 4:
+    case 4://exit
     {
     i=i+1;
     }
@@ -76,19 +69,19 @@ switch(x){
     cout << "Please Choose a Valid option"<<endl;
     break;
 }
-if (x==1){
-       for(int q{};q<b;q++)
-    foo[1].putdata();
+if (x==1){//view tasks(function can be found in task.h)
+       for(int q{1};q<=b;q++)
+    foo[q].putdata();
 }
-else if(x==2){
-  foo[1].getdata();  
+else if(x==2){//add tasks(function can be found in task.h)
+  foo[b].getdata();  
 }
 //Urgent();
 
 
 
 }
-cout<< "Test";
+
 return 0;
 
 }
