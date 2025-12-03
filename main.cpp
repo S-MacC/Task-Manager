b2e80a0a83372a0a3a554081a2d629fbe66982ac
Mattew909784 <C24491592@myTUDublin.ie>
2025-12-03 17:55:23 +0000

    ui changes

--- main.cpp
+++ main.cpp
@@ -2,12 +2,11 @@
 #include <string>
 #include <algorithm>
 #include <fstream>
+#include<vector>
 #include "Task.h"
 #include "User.h"
 #include <windows.h>
-#include <vector>
 #define underline "\033[4m"
-
 using namespace std;
 int total{};
 int x{};
@@ -17,10 +16,16 @@ Task task[10000];//array of tasks
 User test;
 ofstream otaskfile;
 ofstream ouserfile;
-extern string store_user;
+string store_user;
+int cntr{1};
+
+
 int main(){
-cout <<"Welcome to the Task Manager!" <<endl;
-cout <<"Select an account-enter a number 1-5:" <<endl;//This needs to be revised
+ HANDLE h=GetStdHandle(STD_OUTPUT_HANDLE);
+ SetConsoleTextAttribute(h,3);  
+cout << underline  <<"Welcome to the Task Manager!" <<endl;
+cout <<underline <<"Select an account-enter a number 1-5:" <<endl;//This needs to be revised
+SetConsoleTextAttribute(h,7);
 ifstream iuserfile;
 iuserfile.open("u.txt",ios::app);
     if(iuserfile.is_open())
@@ -45,7 +50,9 @@ iuserfile.open("u.txt",ios::app);
       iuserfile.close();
     }
     for(int i{1};i<6;i++){
+      SetConsoleTextAttribute(h,7);
       cout<<i<<user[i].getuser()<<endl;;
+      SetConsoleTextAttribute(h,7);
     }
     ifstream itaskfile;
 itaskfile.open("t.txt",ios::app);
@@ -89,21 +96,29 @@ itaskfile.open("t.txt",ios::app);
    string temp = user[usernum].getuser();
    if(temp.empty())
    {
-    cout<<"Please create user";
+    SetConsoleTextAttribute(h,3);
+    cout<<"Please create user"<<endl;
+    SetConsoleTextAttribute(h,7);
     user[usernum].getdata();
+    cntr++;
   
   }
   else if(!temp.empty())
   {
+    SetConsoleTextAttribute(h,3);
     cout<<"Logging in "<<user[usernum].getuser()<<" Enter password"<<endl;
+    SetConsoleTextAttribute(h,7);
     string password;
     string temppass=user[usernum].getpassword();
     cin >>password;
     if(password==temppass){i++;}
-    else{cout<<"Wrong Password"<<endl;}
+    else{
+      SetConsoleTextAttribute(h,4);
+      cout<<"Wrong Password"<<endl;}
+      SetConsoleTextAttribute(h,7);
   }
   remove("u.txt");
-  for(int i{1};i<=6;i++)
+  for(int i{1};i<=cntr-1;i++)
   {
   ouserfile.open("u.txt",ios::app);
   if(ouserfile.is_open())
@@ -115,6 +130,7 @@ itaskfile.open("t.txt",ios::app);
   
     else
     {
+      SetConsoleTextAttribute(h,4);
       cout<< "bad"<<endl;
     }
   }
@@ -128,14 +144,18 @@ itaskfile.open("t.txt",ios::app);
   string store_user = user[usernum].getuser();
   user[usernum].putdata();
 for(int i{}; i<1; ){//looping the switch statement
+  SetConsoleTextAttribute(h,3);
   cout <<"Select an option: 1 to view all tasks, 2 to add a task, 3 to edit tasks and 4 to exit"<<endl;
+  SetConsoleTextAttribute(h,7);
   cin >> x ; 
     switch(x){
     case 1://see statement below
     break;
     case 2://see statement below
               //updating total number of tasks
+    SetConsoleTextAttribute(h,3);
     cout <<"Add Task"<<endl;
+    SetConsoleTextAttribute(h,7);
     break;
     case 3:
     break;
@@ -145,21 +165,30 @@ for(int i{}; i<1; ){//looping the switch statement
       }
     break;
     default:
+    SetConsoleTextAttribute(h,3);
     cout << "Please Choose a Valid option"<<endl;
+    SetConsoleTextAttribute(h,4);
     break;
     }
 
   if (x==1){//view tasks(function can be found in task.h)
-    for(int i{};i<=total-1;i++){
+    for(int i{};i<=total;i++){
     if(task[i].getuser()==store_user){
-    cout<<"Number: "<<i<<endl;
+    SetConsoleTextAttribute(h,7);
+   cout<<"Number: "<<i<<endl;
     task[i].putdata();
-        
+    SetConsoleTextAttribute(h,3);
+    cout <<"*****************************************************************************************************"<<endl;
     }
   }
 }
   else if(x==2){//add tasks(function can be found in task.h)
-  
+  if(total==9999){
+    SetConsoleTextAttribute(h,4);
+    cout<<"No more tasks";
+  SetConsoleTextAttribute(h,7);}
+    
+  else{
   task[total].getdata();
   string name = user[usernum].getuser();
   task[total].setuser(name);
@@ -176,12 +205,15 @@ for(int i{}; i<1; ){//looping the switch statement
   
     else
     {
+      SetConsoleTextAttribute(h,4);
       cout<< "bad"<<endl;
+      SetConsoleTextAttribute(h,7);
     }
   
   total++;
   
   }
+}
 //Urgent();
 
 
@@ -189,19 +221,27 @@ if (x==3){
    int num{};
    int choice{};
     string input;
+    SetConsoleTextAttribute(h,3);
     cout <<"Edit Tasks"<<endl;//no functionality. Will change to edit tasks
     cout <<"Enter a number"<<endl;//using numbers as a referencing tool
+    SetConsoleTextAttribute(h,7);
     cin >>num;
+    if(task[choice].getuser()==store_user){
+      SetConsoleTextAttribute(h,3);
     cout <<"What do you want to change"<<endl;
     cout <<"1. Name"<<endl;
     cout <<"2. Description"<<endl;
     cout <<"3. Due date"<<endl;
     cout <<"4. Status"<<endl;
     cout <<"5. Mark as complete"<<endl;
+    SetConsoleTextAttribute(h,7);
     cin>>choice;
+    
     switch(choice){
       case 1:
+      SetConsoleTextAttribute(h,3);
       cout <<"Current name is"<<task[num].getname()<<". Please enter your new name"<<endl;
+      SetConsoleTextAttribute(h,7);
       getline(cin,input);
       getline(cin,input);
       task[num].setname(input);
@@ -221,12 +261,16 @@ if (x==3){
         }
         else
         {
+          SetConsoleTextAttribute(h,4);
         cout<< "bad"<<endl;
+        SetConsoleTextAttribute(h,7);
         }
       }
       break;
       case 2:
+      SetConsoleTextAttribute(h,3);
       cout <<"Current description is"<<task[num].getdescription()<<". Please enter your new description"<<endl;
+      SetConsoleTextAttribute(h,7);
       getline(cin,input);
       task[num].setdescription(input);
             remove("t.txt");
@@ -244,7 +288,9 @@ if (x==3){
         }
         else
         {
+          SetConsoleTextAttribute(h,4);
         cout<< "bad"<<endl;
+        SetConsoleTextAttribute(h,7);
         }
       }
       break;
@@ -267,12 +313,16 @@ if (x==3){
         }
         else
         {
+          SetConsoleTextAttribute(h,4);
         cout<< "bad"<<endl;
+        SetConsoleTextAttribute(h,7);
         }
       }
       break;
       case 4:
+      SetConsoleTextAttribute(h,3);
       cout <<"Current status is"<<task[num].getstatus()<<". Please enter your new status"<<endl;
+      SetConsoleTextAttribute(h,7);
       getline(cin,input);
       task[num].setstatus(input);
             remove("t.txt");
@@ -290,12 +340,16 @@ if (x==3){
         }
         else
         {
+          SetConsoleTextAttribute(h,4);
         cout<< "bad"<<endl;
+        SetConsoleTextAttribute(h,7);
         }
       }
       break;
       case 5:
+      SetConsoleTextAttribute(h,3);
       cout <<"Are you sure you want to mark "<<task[num].getstatus()<<" as complete. Doing so will delete all instances of this task[y/n]"<<endl;
+      SetConsoleTextAttribute(h,7);
       string sure{};
       cin>>sure;
         if (sure=="y"||sure=="Y")
@@ -324,17 +378,28 @@ if (x==3){
         }
         else
         {
+          SetConsoleTextAttribute(h,4);
         cout<< "bad"<<endl;
+        SetConsoleTextAttribute(h,7);
         }
       }
           }
         }
         else
         {
+          SetConsoleTextAttribute(h,4);
           cout<<"Your task was not deleted"<<endl;
+          SetConsoleTextAttribute(h,7);
         }
       
     }
+  }
+  else
+  {
+    SetConsoleTextAttribute(h,4);
+    cout<<"You don't have permission to edit this task"<<endl;
+    SetConsoleTextAttribute(h,7);
+  }
   }
 }
 
@@ -353,10 +418,12 @@ if (x==3){
         }
         else
         {
+          SetConsoleTextAttribute(h,4);
         cout<< "bad"<<endl;
+        SetConsoleTextAttribute(h,7);
         }
       }
-
+      
 return 0;
   
 }
