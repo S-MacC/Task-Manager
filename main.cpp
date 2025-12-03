fc7088f1da4a491499ec7c3ee5e5ea5418d8f697
Mattew909784 <C24491592@myTUDublin.ie>
2025-12-03 13:10:01 +0000

    practice color code

--- main.cpp
+++ main.cpp
@@ -3,13 +3,88 @@
 #include <algorithm>
 #include "Task.h"
 #include "User.h"
+#include <windows.h>
+#include <vector>
+#define underline "\033[4m"
+
 using namespace std;
 int total{};
 int x{};
 int usernum{};
 extern string store_user;
+
+size_t get_longest_text(const std::vector<std::string>& lines)
+{
+    size_t ret = 0;
+    for (auto& line : lines)
+    {
+        if (line.size() > ret)
+        {
+            ret = line.size();
+        }
+    }
+    return ret;
+}
+
+void print_border_top_or_bottom_line(size_t longest_text)
+{
+    for (size_t i = 0; i < longest_text + 4; ++i)
+    {
+        std::cout << "*";
+    }
+    std::cout << "\n";
+}
+
+void print_second_top_or_second_bottom_line(size_t longest_text)
+{
+    std::cout << "*";
+    for (size_t i = 0; i < longest_text + 2; ++i)
+    {
+        std::cout << " ";
+    }
+    std::cout << "*";
+    std::cout << "\n";
+}
+
+void print_line(const std::string& text, size_t longest_text)
+{
+    std::cout << "*";
+    std::cout << " ";
+    std::cout << text;
+    std::cout << std::string(longest_text + 1 - text.size(), ' ');
+    std::cout << "*";
+    std::cout << "\n";
+}
+
+void print_lines_with_border(const std::vector<std::string>& lines)
+{
+    size_t longest_text = get_longest_text(lines);
+    print_border_top_or_bottom_line(longest_text);
+    print_second_top_or_second_bottom_line(longest_text);
+    for (auto& line : lines)
+    {
+        print_line(line, longest_text);
+    }
+    print_second_top_or_second_bottom_line(longest_text);
+    print_border_top_or_bottom_line(longest_text);
+}
+
+
+
 int main(){
-cout <<"Welcome to the Task Manager!" <<endl;
+  string name;
+  cout << "Enter your name: "<<endl;
+  cin>> name;
+
+  HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(h,4);
+    std::vector<std::string> lines{name};
+    print_lines_with_border(lines);
+    cout << "\n";
+    SetConsoleTextAttribute(h,2);
+    print_lines_with_border(lines);
+cout <<underline<<"Welcome to the Task Manager!" <<endl;
+    SetConsoleTextAttribute(h,2);
 cout <<"Select an account-enter a number 1-5:" <<endl;//This needs to be revised
 cin >> usernum ; 
   switch(usernum){//user selection
